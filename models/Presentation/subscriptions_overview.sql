28980882b9ce20042b3f1362df4936577240997e
Analytics CalderaLabs <analytics@calderalab.com>
2023-12-05 08:27:54 +0000

    bqeifqe

--- models/Presentation/subscriptions_overview.sql
+++ models/Presentation/subscriptions_overview.sql
@@ -25,7 +25,7 @@ left join {{ ref('dim_platform')}} c
 on a.platform_key = c.platform_key 
 left join (select product_key, product_id, product_name, sku from {{ref('dim_product')}} ) d
 on a.product_key = d.product_key
-left join (select * from {{ ref('dim_subscription')}} where status = 'Active') e
+left join (select * from {{ ref('dim_subscription')}} where status = 'active') e
 on a.subscription_key = e.subscription_key 
 
 
