09e6085462a29925e941e2b52b884c9e4923a28e
Analytics CalderaLabs <analytics@calderalab.com>
2024-01-19 13:16:53 +0000

    sub changes

--- models/Presentation/subscriptions_overview.sql
+++ models/Presentation/subscriptions_overview.sql
@@ -1,31 +1,9 @@
-select 
-distinct
-c.platform_name,
-e.order_channel,
-c.store_name,
-coalesce(product_id,'') product_id,
-coalesce(product_name,'') product_name,
-coalesce(sku,'') sku,
-subscription_id,
-customer_id,
-utm_source,
-utm_medium,
-created_at,
-next_charge_scheduled_at,
-order_interval_frequency,
-order_interval_unit,
-cancellation_reason,
-cancelled_at,
-order_day_of_month,
-presentment_currency,
-cancellation_reason_comments
-from {{ ref('fact_order_lines')}} a
-
-left join {{ ref('dim_platform')}} c
-on a.platform_key = c.platform_key 
-left join (select product_key, product_id, product_name, sku from {{ref('dim_product')}} ) d
-on a.product_key = d.product_key
-left join (select * from {{ ref('dim_subscription')}} where status = 'active') e
-on a.subscription_key = e.subscription_key 
-
-
+SELECT
+  *EXCEPT(cancelled_at),
+  DATE(cancelled_at) cancelled_at
+FROM
+  {{ ref('dim_subscription_recharge') }}
+WHERE
+  created_at != "2023-10-10"
+  OR status != 'cancelled'
+  OR DATE(cancelled_at) IS NOT NULL
